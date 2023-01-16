36647156ca942e63838f25df5b8c4bc591896801
gustjd887 <gustjd887@gmail.com>
2023-01-16 10:34:24 +0900

    section6

---
+++ 006.section/001.bool/bool.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// var x bool
+
+func main () {
+	// a := 7
+	a := 40
+	b := 42
+
+	// fmt.Println(a == b)
+	// fmt.Println(a <= b)
+	fmt.Println(a != b)
+	// x = true
+	// fmt.Println(x)
+}
---
+++ 006.section/001.bool/readme.md
@@ -0,0 +1,12 @@
+# boolean
+true or false
+
+# bool default value
+false
+
+# = 와 ==
+* = : 지정
+* == : 같음
+* === : go에는 없음...(아주 아주 같음?)
+
+# 사전에 정의된 boolean type을 bool이라고 함
