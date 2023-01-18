d989e664ac13d4d016fe8b22d98e78e3b36f9330
gustjd887 <gustjd887@gmail.com>
2023-01-18 14:42:33 +0900

    8-9

---
+++ 008.section/007.if/if2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	if x:= 42; x == 2 {
+		fmt.Println("001")
+	}
+	fmt.Println("1 line"); fmt.Println("2 statement")
+	// fmt.Println(x)
+}
---
+++ 008.section/008.ifElse/ifelse.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 40
+	if x == 40 {
+		fmt.Println("our value was 41")
+	} else if x == 41 {
+		fmt.Println("our value was 41")
+	} else {
+		fmt.Println("our value was not 40, 41")
+	}
+	
+}
