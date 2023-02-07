fba1e9062c8106fa208901eaf8189ee1d71b8c00
gustjd887 <gustjd887@gmail.com>
2023-02-08 03:26:32 +0900

    14-5

---
+++ 014.section/005.method/method.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func (s secretAgent) speak() {
+	fmt.Println("I am", s.first, s.last)
+}
+
+func main() {
+	sa1 := secretAgent{
+		person: person{
+			"James",
+			"Bond",
+		},
+		ltk: true,
+	}
+
+	sa2 := secretAgent{
+		person: person{
+			"Miss",
+			"Moneypenny",
+		},
+		ltk: true,
+	}
+	// sa3 := person{
+	// 	first: "hs",
+	// 	last: "p",
+	// }
+	fmt.Println(sa1)
+	sa1.speak()
+	sa2.speak()
+	// sa3.speak()
+}
