3b5be650350d2db0a604fc35e189ff3a6184579a
gustjd887 <gustjd887@gmail.com>
2023-02-14 00:58:52 +0900

    22-7

---
+++ 022.section/007.fanIn/001/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	even := make(chan int)
+	odd := make(chan int)
+	fanin := make(chan int)
+
+	go send(even, odd)
+	go receive(even, odd, fanin)
+
+	for v := range fanin {
+		fmt.Println(v)
+	}
+}
+
+func send(e, o chan<- int) {
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			e <- i
+		} else {
+			o <- i
+		}
+	}
+	close(e)
+	close(o)
+}
+
+func receive(e, o <-chan int, fi chan<- int) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		for v := range e {
+			fi <- v
+		}
+		wg.Done()
+	}()
+	go func() {
+		for v := range o {
+			fi <- v
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+	close(fi)
+}
