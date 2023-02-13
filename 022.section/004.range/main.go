0925d31b12993f20ea73295ad311c40d64e79588
gustjd887 <gustjd887@gmail.com>
2023-02-13 23:33:00 +0900

    22-5

--- 022.section/004.range/main.go
+++ 022.section/004.range/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	c := make(chan int)
-	fmt.Println(&c)
+	// fmt.Println(&c)
 	go foo(c) // 밑에 있는 for문이랑 별개로, 계속 체널에 값을 송신하는 역할.
 
 	for v := range c { // range에서 체널 c를 사용하면, 체널이 닫힐 때 까지 체널에서 값을 뺀다.
